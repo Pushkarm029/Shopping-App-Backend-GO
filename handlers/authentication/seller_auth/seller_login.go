fba05451bba0f47b11bb24d32cede16cfd056ba8
Pushkar Mishra <pushkarmishra029@gmail.com>
2023-09-20 17:15:21 +0530

    implemented JWT

--- handlers/authentication/seller_auth/seller_login.go
+++ handlers/authentication/seller_auth/seller_login.go
@@ -4,8 +4,11 @@ import (
 	"amazon-backend/config"
 	"amazon-backend/models"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -34,5 +37,18 @@ func Login(c *gin.Context) {
 		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid password"})
 		return
 	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": seller.Email,
+		"exp":   time.Now().Add(time.Hour * 24 * 30).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Failed to create Token"})
+	}
+
+	c.SetSameSite(http.SameSiteLaxMode)
+	c.SetCookie("SellerAuthorization", tokenString, 60*60*24*30, "", "", false, true)
 	c.JSON(http.StatusOK, gin.H{"message": "Login successful"})
 }
--- main.go
+++ main.go
@@ -6,11 +6,11 @@ import (
 	userauth "amazon-backend/handlers/authentication/user_auth"
 	sellerhandler "amazon-backend/handlers/seller_handler"
 	userhandler "amazon-backend/handlers/user_handler"
+	"amazon-backend/middleware"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
-
 
 func main() {
 	config.Connect()
@@ -25,15 +25,15 @@ func initRoutes(r *gin.Engine) {
 	r.POST("/api/user/register", userauth.Register)
 	r.POST("/api/seller/login", sellerauth.Login)
 	r.POST("/api/seller/register", sellerauth.Register)
-	r.POST("/api/seller/addproduct", sellerhandler.AddProduct)
-	r.GET("/api/seller/getproduct", sellerhandler.Get_Product)
+	r.POST("/api/seller/addproduct", middleware.RequireSellerAuth, sellerhandler.AddProduct)
+	r.GET("/api/seller/getproduct", middleware.RequireSellerAuth, sellerhandler.Get_Product)
 	r.GET("/api/getallproducts", userhandler.Search_Product)
-	r.DELETE("/api/seller/delete", sellerhandler.RemoveProduct)
-	r.PUT("/api/seller/update", sellerhandler.Update_Product)
+	r.DELETE("/api/seller/delete", middleware.RequireSellerAuth, sellerhandler.RemoveProduct)
+	r.PUT("/api/seller/update", middleware.RequireSellerAuth, sellerhandler.Update_Product)
 	r.POST("/api/user/addcart", userhandler.Add_Item_In_Cart)
 	r.DELETE("/api/user/removecart", userhandler.Remove_Item_In_Cart)
 	r.GET("/api/user/viewcart", userhandler.View_Item_In_Cart)
-	r.POST("/api/user/placeorder", userhandler.Place_Order)
-	r.DELETE("/api/user/cancelorder", userhandler.Cancel_Order)
-	r.GET("/api/user/vieworder", userhandler.View_Purchased_Items)
+	r.POST("/api/user/placeorder", middleware.RequireUserAuth, userhandler.Place_Order)
+	r.DELETE("/api/user/cancelorder", middleware.RequireUserAuth, userhandler.Cancel_Order)
+	r.GET("/api/user/vieworder", middleware.RequireUserAuth, userhandler.View_Purchased_Items)
 }
